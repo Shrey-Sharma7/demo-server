73c23da1a30fb101142d5e43cdf2e3cdc29daeb7
Shrey Sharma <cr7shrey25@gmail.com>
2023-04-10 19:10:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser')
-const cors = require('cors');
-const morgan = require('morgan');
-const helmet = require('helmet');
 const fs = require('fs');
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-
-app.use(cors());
-app.use(morgan());
-app.use(helmet());
 
 app.post('/webhook', (req, res) => {
   console.log('Received webhook:', req.body);
